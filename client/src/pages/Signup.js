40e955b8718865305b54444972bef046654a4fd6
Chandler Liang <chandler.liang123@gmail.com>
2021-10-31 21:17:42 +1100

    stylised the sign up page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
 import { ADD_USER } from '../gql/mutations';
-
+import img from '../assets/images/banquet.jpg'
+import logo from '../assets/images/foodyLogo.png'
 
 function Signup(props) {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
@@ -32,45 +33,51 @@ function Signup(props) {
 
   return (
     <>
+      <div className='ui three column doubling stackable grid container'>
+        <div className='column'>
+          <img src={logo}></img>
+          <h2>Sign up</h2>
+          <form className="ui form" onSubmit={handleFormSubmit}>
+            <div className="field">
+              <label htmlFor="username">Username:</label>
+              <input
+                placeholder="username"
+                name="username"
+                type="text"
+                id="username"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="email">Email:</label>
+              <input
+                placeholder="youremail@test.com"
+                name="email"
+                type="email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
 
-      <h2>Sign UP</h2>
-      <form className="ui form" onSubmit={handleFormSubmit}>
-        <div className="field">
-          <label htmlFor="username">Username:</label>
-          <input
-            placeholder="username"
-            name="username"
-            type="text"
-            id="username"
-            onChange={handleChange}
-          />
+            <div className="field">
+              <label htmlFor="pwd">Password:</label>
+              <input
+                placeholder="******"
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="flex-row flex-end">
+              <button type="submit">Submit</button>
+            </div>
+          </form>
         </div>
-        <div className="field">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
+        <div className='column'>
+          <img src={img} />
         </div>
-
-        <div className="field">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-
+      </div>
     </>
   );
 }
