6143ecb834839885557c4ed122205a4a6919d321
Chandler Liang <chandler.liang123@gmail.com>
2021-10-31 21:24:16 +1100

    stylised the login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN } from '../gql/mutations';
 import Auth from '../utils/auth';
+import img from '../assets/images/banquet.jpg'
+import logo from '../assets/images/foodyLogo.png'
 
 
 function Login(props) {
@@ -31,38 +33,46 @@ function Login(props) {
 
   return (
     <>
+      <div className='ui three column doubling stackable grid container'>
+        <div className='column'>
+          <img src={logo}></img>
+          <h2>Login</h2>
+          <form className="ui form" onSubmit={handleFormSubmit}>
+            <div className="field">
+              <label htmlFor="email">Email address:</label>
+              <input
+                placeholder="youremail@test.com"
+                name="email"
+                type="email"
+                id="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="pwd">Password:</label>
+              <input
+                placeholder="******"
+                name="password"
+                type="password"
+                id="pwd"
+                onChange={handleChange}
+              />
+            </div>
+            {error ? (
+              <div>
+                <p className="error-text">The provided credentials are incorrect</p>
+              </div>
+            ) : null}
+            <div className="flex-row flex-end">
+              <button type="submit">Submit</button>
+            </div>
+          </form>
+        </div>
+        <div className='column'>
+          <img src={img} />
+        </div>
+      </div>
 
-      <h2>login</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email address:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        {error ? (
-          <div>
-            <p className="error-text">The provided credentials are incorrect</p>
-          </div>
-        ) : null}
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
 
     </>
   );
